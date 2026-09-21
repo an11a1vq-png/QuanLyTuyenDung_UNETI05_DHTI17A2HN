fe65250d6beac85a11b96987beb8d937aacd9332
Nguyen Khac Minh Hieu <nvkhanh.dhti17a2hn@sv.uneti.edu.vn>
2026-09-21 08:46:48 +0700

    23103100101_NguyenVanKhanh test push

--- QuanLyTuyenDung_UNETI05_DHTI17A2HN/Models/ErrorViewModel.cs
+++ QuanLyTuyenDung_UNETI05_DHTI17A2HN/Models/ErrorViewModel.cs
@@ -3,7 +3,7 @@ namespace QuanLyTuyenDung_UNETI05_DHTI17A2HN.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-
+        //test push
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
